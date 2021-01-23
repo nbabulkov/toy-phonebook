bc35f162a0b090bab6b04f0ab58dc7c35bdbd65a
nbabulkov <nbabulkov@gmail.com>
2021-01-23 14:11:17 +0200

    Add CORS package

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const morgan = require('morgan');
+const cors = require('cors');
+
 const app = express();
 
 app.use(express.json());
 app.use(morgan('tiny'));
+app.use(cors());
 
 let phonebook = [
   {
@@ -70,7 +73,6 @@ app.post('/api/phonebook', (request, response) => {
     } else {
       person.id = maxId + 1;
       phonebook = phonebook.concat(person);
-      console.log(phonebook);
       response.json(person);
     }
 });
